bae0530eab6708ca9ee5bdc1f80aa313dc531de9
Quasi <vbaranov@russoutdoor.ru>
2025-08-26 21:53:29 +0300

    bat file

---
+++ en/useful_clicker/UsefulClicker_2.0/run_voice_demo.bat
@@ -0,0 +1,7 @@
+rem python main.py examples/anymusic_youtube.xml --debug
+rem python main.py examples/curiosity_drive_test_broad_topics.xml --debug
+python main.py examples/voice_demo2.xml --debug
+set VOICE_DEVICE = 1
+set VOICE_WAIT_SECONDS = 60
+set DEBUG_DUMP_VOICE = 1
+rem python usefulclicker_runner.py
