f1f1316588e30a536e930f7eba21cada3f2487c9
Quasi <vbaranov@russoutdoor.ru>
2025-08-30 10:59:11 +0300

    postirony prompt

---
+++ en/useful_clicker/UsefulClicker_2.0/run_postirony.bat
@@ -0,0 +1,3 @@
+rem python main.py examples/anymusic_youtube.xml --debug
+rem python main.py examples/curiosity_drive_test_broad_topics.xml --debug
+python main.py examples/curiosity_postirony.xml --debug
