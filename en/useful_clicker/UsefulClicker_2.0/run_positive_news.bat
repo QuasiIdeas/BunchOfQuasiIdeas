ece739e33c4126d1af439f39530c7a946fc3bd23
Quasi <vbaranov@russoutdoor.ru>
2025-08-31 18:12:58 +0300

    UsefulClicker moved to new repository

--- en/useful_clicker/UsefulClicker_2.0/run_positive_news.bat
+++
@@ -1,3 +0,0 @@
-rem python main.py examples/anymusic_youtube.xml --debug
-rem python main.py examples/curiosity_drive_test_broad_topics.xml --debug
-python main.py examples/positive_news.xml --debug
